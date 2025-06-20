ac67f1038722c8aa8b5082ad72111777cd215db2
Arturo Curahua <20141740@aloe.ulima.edu.pe>
2025-06-19 19:28:03 -0500

    Creación de la clase Datos

---
+++ EDA_Proyecto/src/Clases/Datos.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Datos {
+    private String dni;
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Datos(String dni, String nombre, String telefono, String email) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
